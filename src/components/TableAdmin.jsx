557fbc3de4d60893ec6466fadc166a978022c457
irfaan <irfanyazid18@gmail.com>
2024-02-12 14:02:08 +0700

    edit modal admin

--- src/components/TableAdmin.jsx
+++ src/components/TableAdmin.jsx
@@ -2,10 +2,12 @@ import React,{Fragment,useState} from 'react'
 import Layout from '../layout/Layout'
 import CreateAdmin from '../pages/CreateAdmin'
 import Button from './Button'
+import UpdateAdmin from '../pages/UpdateAdmin'
 
 export default function TableAdmin() {
 
   const [visible,setVisible] = useState(false)
+  const [showEdit,setShowEdit] = useState(false)
 
   
   return (
@@ -15,15 +17,27 @@ export default function TableAdmin() {
           <table className="w-full text-sm text-left rtl:text-right text-dark-purple dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-dark-purple dark:text-gray-400">
                   <tr>
-                      <th scope="col" className="px-6 py-3">
+                      <th scope="col" className="px-6 py-3 text-center">
                           Username
                       </th>
                       
-                      <th scope="col" className="px-6 py-3">
+                      <th scope="col" className="px-6 py-3 text-center">
                           Password
                       </th>
-                      <th scope="col" className="px-6 py-3">
+                      <th scope="col" className="px-6 py-3 text-center">
                           Role
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                          Email
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                          Name
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                          Phone
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                          Address
                       </th>
                       <th scope="col" className="px-6 py-3 text-center">
                           Action
@@ -43,8 +57,23 @@ export default function TableAdmin() {
                           Admin
                       </td>
                       <td className="px-6 py-4 text-black">
+                          hanni@gmail.com
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                          hanni
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                          087734293487
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                          Vietnam
+                      </td>
+                      <td className="px-6 py-4 text-black">
+                       <Button onClick={()=>{setShowEdit(true)}}>
+                        Edit
+                        </Button>
                        <Button>
-                          delete
+                          Delete
                        </Button>
                       </td>
                   </tr>
@@ -52,9 +81,10 @@ export default function TableAdmin() {
           </table>
       </div>
 
-      <button className='text-black text-xl place-self-end' onClick={()=>{setVisible(true)}}>buka</button>
+      <button className='text-black text-xl place-self-end' onClick={()=>{setVisible(true)}}>Add</button>
 
       <CreateAdmin visible={visible} setVisible={setVisible}/>
+      <UpdateAdmin showEdit={showEdit} setShowEdit={setShowEdit}/>
       </Fragment>
 
     </Layout>
--- src/pages/UpdateAdmin.jsx
+++ src/pages/UpdateAdmin.jsx
@@ -1,13 +1,13 @@
 import React,{useState} from 'react'
 import { Modal } from '../components/Modal'
 
-export default function CreateAdmin({visible, setVisible}) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [address, setAddress] = useState('');
+export default function UpdateAdmin({showEdit, setShowEdit}) {
+  const [username, setUsername] = useState('hanni');
+  const [password, setPassword] = useState('hanniimut');
+  const [email, setEmail] = useState('hanni@gmail.com');
+  const [name, setName] = useState('hanni');
+  const [phone, setPhone] = useState('08723423871');
+  const [address, setAddress] = useState('Vietnam');
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -41,9 +41,9 @@ export default function CreateAdmin({visible, setVisible}) {
     setVisible(false);
   };
   return (
-    <Modal visible={visible} setVisible={setVisible} content={
+    <Modal visible={showEdit} setVisible={setShowEdit} content={
       <div>
-       <h2 className='text-lg font-semibold mb-4'>Create Admin</h2>
+       <h2 className='text-lg font-semibold mb-4'>Update Admin</h2>
         <div className='mb-4'>
           <label className='block text-sm text-gray-700' htmlFor='username'>
             Username
@@ -84,7 +84,7 @@ export default function CreateAdmin({visible, setVisible}) {
             name
           </label>
           <input
-            id='name'
+            id='email'
             className='w-full px-4 py-2 border rounded-md'
             value={name}
             onChange={handleNameChange}
